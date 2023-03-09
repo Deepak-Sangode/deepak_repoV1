e5367d83b1df997f510785e56d27ad8f119a9870
Deepak291089 <dpsangode29@gmail.com>
2023-03-09 22:52:13 +0530

    first commit

---
+++ deepak_project/src/testPackage/TestBase.java
@@ -0,0 +1,13 @@
+package testPackage;
+
+public class TestBase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("TestBase Class");
+		System.out.println("POM.xml");
+		System.out.println("Util");
+	}
+
+}
